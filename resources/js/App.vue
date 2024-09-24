4221cbdaf3cb5347b0f5f1a127e5859f06ac98a2
ilhamhafidz404 <xxspanzie@gmail.com>
2024-09-24 11:38:12 +0700

    Lets Build Something Awesome

---
+++ resources/js/App.vue
@@ -0,0 +1,14 @@
+<template>
+    <h1>Test</h1>
+    <router-link to="/">Home</router-link>
+    <router-link to="/about">About</router-link>
+    <router-view></router-view>
+</template>
+
+<script>
+export default {
+    name: "App",
+};
+</script>
+
+<style></style>
---
+++ resources/js/pages/Home.vue
@@ -0,0 +1,9 @@
+<template>
+    <h2>fywe</h2>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
